92b0733618db2155d3f067407c937891fe70317a
atsushi takahagi <takahagi@manavista.jp>
2017-06-25 11:49:15 +0900

    feat: implements toolbar to setting screen

--- app/src/main/java/jp/manavista/developbase/view/activity/SettingActivity.java
+++ app/src/main/java/jp/manavista/developbase/view/activity/SettingActivity.java
@@ -1,9 +1,10 @@
 package jp.manavista.developbase.view.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+import jp.manavista.developbase.R;
 import jp.manavista.developbase.view.fragment.SettingFragment;
 
 /**
@@ -16,14 +17,17 @@ import jp.manavista.developbase.view.fragment.SettingFragment;
  * </p>
  * @see SettingFragment
  */
-public class SettingActivity extends Activity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_setting);
+
         getFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, SettingFragment.newInstance())
+                .replace(R.id.content_setting, SettingFragment.newInstance())
                 .commit();
 
     }
