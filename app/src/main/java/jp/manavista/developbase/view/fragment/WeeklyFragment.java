3825d493be646ec851ce6c951ada433080712422
atsushi takahagi <takahagi@manavista.jp>
2017-06-29 15:33:01 +0900

    feat: Create timetable dynamically

--- app/src/main/java/jp/manavista/developbase/view/fragment/WeeklyFragment.java
+++ app/src/main/java/jp/manavista/developbase/view/fragment/WeeklyFragment.java
@@ -2,13 +2,16 @@ package jp.manavista.developbase.view.fragment;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -87,16 +90,31 @@ public final class WeeklyFragment extends Fragment {
         tableLayout.addView(row);
         tableLayout.addView(row2);
 
+        buildContents(tableLayout, contents, rootView);
+
         return rootView;
     }
 
+    /**
+     *
+     * Build Day Header
+     *
+     * <p>
+     * Overview:<br>
+     * Generate the date part of the header.
+     * </p>
+     *
+     * @param context Context
+     * @param displayDays display days array
+     * @return TableRow Header
+     */
     private TableRow buildDayHeader(Context context, String[] displayDays) {
 
         TableRow tableRow = new TableRow(context);
 
-        TableRow.LayoutParams params = new TableRow.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        tableRow.setLayoutParams(params);
+//        TableRow.LayoutParams params = new TableRow.LayoutParams(
+//                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+//        tableRow.setLayoutParams(params);
 
         TextView originDay = new TextView(context);
 
@@ -124,13 +142,26 @@ public final class WeeklyFragment extends Fragment {
         return tableRow;
     }
 
+    /**
+     *
+     * Build Days of week Header
+     *
+     * <p>
+     * Overview:<br>
+     * Generate the days of week part of the header.
+     * </p>
+     *
+     * @param context Context
+     * @param displayDays display days of week array
+     * @return TableRow Header
+     */
     private TableRow buildDaysOfWeekHeader(Context context, String[] displayDays) {
 
         TableRow tableRow = new TableRow(context);
 
         TableRow.LayoutParams params = new TableRow.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        tableRow.setLayoutParams(params);
+//        tableRow.setLayoutParams(params);
 
         TextView originDay = new TextView(context);
         tableRow.addView(originDay);
@@ -154,5 +185,66 @@ public final class WeeklyFragment extends Fragment {
         tableRow.addView(nullDay);
 
         return tableRow;
+    }
+
+    private void buildContents(TableLayout layout, Context context, View rootView) {
+
+        // TODO: First, create timetable structure.
+
+        TableRow row = new TableRow(context);
+        TableRow.LayoutParams tableRowParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
+        row.setBackgroundResource(R.color.lightGray);
+
+        // add row to timetable
+
+        RelativeLayout timetableLayout = new RelativeLayout(context);
+        timetableLayout.setLayoutParams(tableRowParams);
+
+        final int margin4dp = getResources().getDimensionPixelSize(R.dimen.margin_4dp);
+
+        TextView begin = new TextView(context);
+        begin.setPadding(0, margin4dp, 0, 0);
+
+        begin.setText("00:00");
+        timetableLayout.addView(begin);
+
+        TextView lessonNo = new TextView(context);
+        RelativeLayout.LayoutParams lessonNoParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        lessonNoParams.addRule(RelativeLayout.CENTER_HORIZONTAL, lessonNo.getId());
+        lessonNoParams.addRule(RelativeLayout.CENTER_VERTICAL, lessonNo.getId());
+        lessonNo.setLayoutParams(lessonNoParams);
+        lessonNo.setText("1");
+        lessonNo.setTextColor(ContextCompat.getColor(context, R.color.firebrick));
+        lessonNo.setTypeface(lessonNo.getTypeface(), Typeface.BOLD);
+        timetableLayout.addView(lessonNo);
+
+        TextView end = new TextView(context);
+        RelativeLayout.LayoutParams endParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        endParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, end.getId());
+        endParams.addRule(RelativeLayout.ALIGN_PARENT_START, end.getId());
+
+        end.setPadding(0, 0, 0, margin4dp);
+        end.setText("99:99");
+        end.setLayoutParams(endParams);
+        timetableLayout.addView(end);
+
+        row.addView(timetableLayout);
+
+        // add row to lesson data (loop)
+
+
+
+        // add row to null value
+        TextView nullDay = new TextView(context);
+        row.addView(nullDay);
+
+        TableLayout.LayoutParams tableLayoutParams = new TableLayout.LayoutParams(
+                TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT, 1.0f);
+        tableLayoutParams.setMargins(0, margin4dp, 0, margin4dp);
+
+        layout.addView(row, tableLayoutParams);
     }
 }
